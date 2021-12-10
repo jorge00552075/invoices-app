d8e29ee750456474899dc2a022968578854038f4
Jorge Sendejo <jorge00552075@gmail.com>
2021-12-09 23:43:44 -0600

    added form validation using the react-hook-form library

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,20 +1,33 @@
+import { useForm } from 'react-hook-form';
+
 import { ReactComponent as Delete } from '../../assets/icon-delete.svg';
 
 import styles from './Form.module.css';
 
 function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitted, isValid, errors },
+  } = useForm({ mode: 'onBlur' });
+
+  console.log('errors', errors);
+
   function handleClick(e) {
     console.log(e.target.innerText);
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log('FORM SUBMITTED');
+  function onSubmit(data) {
+    console.log(data);
   }
 
+  const formControl = styles['form-control'];
+  const formControlBorderRed = `${styles['form-control']} ${styles['border-red']}`;
+
+  // prettier-ignore
   return (
     <div className={styles['form-wrapper']}>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h2 className={styles['form-heading']}>New Invoice</h2>
         <div className={styles['fieldset-wrapper']}>
           <fieldset>
@@ -23,8 +36,12 @@ function Form() {
               <label htmlFor="senderAddress.street">Street Address</label>
               <input
                 id="senderAddress.street"
-                name="senderAddress.street"
-                className={styles['form-control']}
+                {...register('senderAddress.street', { required: true })}
+                className={
+                  errors.senderAddress?.street
+                    ? formControlBorderRed
+                    : formControl
+                }
               />
             </div>
             <div className={styles['form-row']}>
@@ -32,53 +49,72 @@ function Form() {
                 <label htmlFor="senderAddress.city">City</label>
                 <input
                   id="senderAddress.city"
-                  name="senderAddress.city"
-                  className={styles['form-control']}
+                  {...register('senderAddress.city', { required: true })}
+                  className={
+                    errors.senderAddress?.city
+                      ? formControlBorderRed
+                      : formControl
+                  }
                 />
               </div>
               <div className={styles['form-group']}>
                 <label htmlFor="senderAddress.postCode">Post Code</label>
                 <input
                   id="senderAddress.postCode"
-                  name="senderAddress.postCode"
-                  className={styles['form-control']}
+                  {...register('senderAddress.postCode', { required: true })}
+                  className={
+                    errors.senderAddress?.postCode
+                      ? formControlBorderRed
+                      : formControl
+                  }
                 />
               </div>
               <div className={styles['form-group']}>
                 <label htmlFor="senderAddress.country">Country</label>
                 <input
                   id="senderAddress.country"
-                  name="senderAddress.country"
-                  className={styles['form-control']}
+                  {...register('senderAddress.country', { required: true })}
+                  className={
+                    errors.senderAddress?.country
+                      ? formControlBorderRed
+                      : formControl
+                  }
                 />
               </div>
             </div>
           </fieldset>
-
           <fieldset>
             <legend>Bill To</legend>
             <div className={styles['form-group']}>
               <label htmlFor="clientName">Client's Name</label>
               <input
                 id="clientName"
-                name="clientName"
-                className={styles['form-control']}
+                {...register('clientName', { required: true })}
+                className={
+                  errors.clientName ? formControlBorderRed : formControl
+                }
               />
             </div>
             <div className={styles['form-group']}>
               <label htmlFor="clientEmail">Client's Email</label>
               <input
                 id="clientEmail"
-                name="clientEmail"
-                className={styles['form-control']}
+                {...register('clientEmail', { required: true, pattern: /^\S+@\S+$/i })}
+                className={
+                  errors.clientEmail ? formControlBorderRed : formControl
+                }
               />
             </div>
             <div className={styles['form-group']}>
               <label htmlFor="clientAddress.street">Street Address</label>
               <input
                 id="clientAddress.street"
-                name="clientAddress.street"
-                className={styles['form-control']}
+                {...register('clientAddress.street', { required: true })}
+                className={
+                  errors.clientAddress?.street
+                    ? formControlBorderRed
+                    : formControl
+                }
               />
             </div>
             <div className={styles['form-row']}>
@@ -86,24 +122,36 @@ function Form() {
                 <label htmlFor="clientAddress.city">City</label>
                 <input
                   id="clientAddress.city"
-                  name="clientAddress.city"
-                  className={styles['form-control']}
+                  {...register('clientAddress.city', { required: true })}
+                  className={
+                    errors.clientAddress?.city
+                      ? formControlBorderRed
+                      : formControl
+                  }
                 />
               </div>
               <div className={styles['form-group']}>
                 <label htmlFor="clientAddress.postCode">Post Code</label>
                 <input
                   id="clientAddress.postCode"
-                  name="clientAddress.postCode"
-                  className={styles['form-control']}
+                  {...register('clientAddress.postCode', { required: true })}
+                  className={
+                    errors.clientAddress?.postCode
+                      ? formControlBorderRed
+                      : formControl
+                  }
                 />
               </div>
               <div className={styles['form-group']}>
                 <label htmlFor="clientAddress.country">Country</label>
                 <input
                   id="clientAddress.country"
-                  name="clientAddress.country"
-                  className={styles['form-control']}
+                  {...register('clientAddress.country', { required: true })}
+                  className={
+                    errors.clientAddress?.country
+                      ? formControlBorderRed
+                      : formControl
+                  }
                 />
               </div>
             </div>
@@ -115,21 +163,27 @@ function Form() {
                 <input
                   type="date"
                   id="createdAt"
-                  name="createdAt"
-                  className={styles['form-control']}
+                  {...register('createdAt', { required: true })}
+                  className={
+                    errors.createdAt ? formControlBorderRed : formControl
+                  }
                 />
               </div>
               <div className={styles['form-group']}>
                 <label htmlFor="paymentTerms">Payment Terms</label>
                 <select
+                  defaultValue={30}
                   id="paymentTerms"
-                  name="paymentTerms"
-                  className={styles['form-control']}
+                  {...register('paymentTerms', { required: true })}
+                  className={
+                    errors.paymentTerms ? formControlBorderRed : formControl
+                  }
                 >
                   <option value="1">Net 1 Day</option>
                   <option value="7">Net 7 Day</option>
                   <option value="14">Net 14 Day</option>
                   <option value="30">Net 30 Day</option>
+                  {/* ADD DEFAULT OF 30 */}
                 </select>
               </div>
             </div>
@@ -137,43 +191,47 @@ function Form() {
               <label htmlFor="description">Description</label>
               <input
                 id="description"
-                name="description"
-                className={styles['form-control']}
+                {...register('description', { required: true })}
+                className={
+                  errors.description ? formControlBorderRed : formControl
+                }
               />
             </div>
           </fieldset>
+          {/* Item List */}
           <fieldset>
-            <legend className={styles['items-heading']}>ItemList</legend>
+            <legend className={styles['items-heading']}>Item List</legend>
             <div className={`${styles['form-row']} ${styles['col-5']}`}>
               <div className={styles['form-group']}>
-                <label htmlFor="items[0].name">Item Name</label>
+                <label htmlFor="items.0.name">Item Name</label>
                 <input
-                  id="items[0].name"
-                  name="items[0].name"
+                  id="items.0.name"
+                  {...register('items.0.name', { required: true })}
                   className={styles['form-control']}
                 />
               </div>
               <div className={styles['form-group']}>
-                <label htmlFor="items[0].quantity">Qty.</label>
+                <label htmlFor="items.0.quantity">Qty.</label>
                 <input
-                  id="items[0].quantity"
-                  name="items[0].quantity"
+                  id="items.0.quantity"
+                  name="items.0.quantity"
+                  {...register('items.0.quantity', { required: true })}
                   className={styles['form-control']}
                 />
               </div>
               <div className={styles['form-group']}>
-                <label htmlFor="items[0].price">Price</label>
+                <label htmlFor="items.0.price">Price</label>
                 <input
                   id="items[0].price"
-                  name="items[0].price"
+                  {...register('items.0.price', { required: true })}
                   className={styles['form-control']}
                 />
               </div>
               <div className={styles['form-group']}>
-                <label htmlFor="items[0].total">Total</label>
+                <label htmlFor="items.0.total">Total</label>
                 <input
-                  id="items[0].total"
-                  name="items[0].total"
+                  id="items.0.total"
+                  name="items.0.total"
                   className={styles['form-control']}
                   disabled
                 />
@@ -187,6 +245,13 @@ function Form() {
             </button>
           </fieldset>
         </div>
+        {/* Error Messages */}
+        <div className={styles.messages}>
+          {errors.clientEmail && <small>- Please enter a valid email</small>}
+          {isSubmitted && !isValid && (<small>- All fields must be added.</small>)}
+        </div>
+
+        {/* Buttons */}
         <div className={styles.buttons}>
           <button
             type="button"
